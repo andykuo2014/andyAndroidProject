6ca9b87cf294465cfdaa236bc27494f79392cb48
andy <andy@kuobrothers.com>
2014-10-02 18:04:41 +0800

    tttt
    ttttt

--- crazy/src/tw/com/newstars/crazy/MainActivity.java
+++ crazy/src/tw/com/newstars/crazy/MainActivity.java
@@ -1,7 +1,12 @@
 package tw.com.newstars.crazy;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import com.facebook.UiLifecycleHelper;
+import com.facebook.widget.FacebookDialog;
 
 import android.app.Activity;
 import android.content.Context;
@@ -9,9 +14,14 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.util.Base64;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -27,13 +37,13 @@ import android.widget.TextView;
 public class MainActivity extends Activity {
 
 	private ScreenDetectBroadcastReceiver broadcastReceiver;
-	
+	private UiLifecycleHelper uiHelper;
 	SharedPreferences sp1;
 	SharedPreferences sp2;
 	Editor editor1;
 	Editor editor2;
 	CheckBox checkNotice;
-	ImageView imageView1; //the share button img.
+	ImageView shareImageView; //the share button img.
 	TextView textView1; //the today usage text.
 	TextView textView3; //the yesterday usage text.
 	SwipeRefreshLayout swipeContainer;
@@ -53,7 +63,7 @@ public class MainActivity extends Activity {
 		editor2 = sp2.edit();
 		textView1 = (TextView) findViewById(R.id.textView1);
 		textView3 = (TextView) findViewById(R.id.textView3);
-		imageView1 = (ImageView)findViewById(R.id.imageView1);
+		shareImageView = (ImageView)findViewById(R.id.shareImageView);
 		
 		swipeContainer = (SwipeRefreshLayout)findViewById(R.id.swipe_container);
 		swipeContainer.setColorSchemeResources (android.R.color.holo_blue_dark, android.R.color.holo_blue_light, android.R.color.holo_green_light, android.R.color.holo_green_light);
@@ -71,12 +81,15 @@ public class MainActivity extends Activity {
 		});
 		
 			
-		imageView1.setOnClickListener(new OnClickListener() {
+		shareImageView.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				editor1.clear().commit();
-				editor2.clear().commit(); 
+				//FB share here.
+				FacebookDialog shareDialog = new FacebookDialog.ShareDialogBuilder(MainActivity.this)
+		        .setLink("https://developers.facebook.com/android")
+		        .build();
+				uiHelper.trackPendingDialogCall(shareDialog.present());
 			}
 		});	
 		
@@ -92,11 +105,32 @@ public class MainActivity extends Activity {
 			Intent i = new Intent(OnBootReceiver.ACTION_HANDLE_EVENTS);
 			sendBroadcast(i);
 		}
+		
+		//printKeyHash(this);
+		
+		uiHelper = new UiLifecycleHelper(this, null);
+	    uiHelper.onCreate(savedInstanceState);
 	}
 	
 	protected void onResume(){
 		super.onResume();
+		uiHelper.onDestroy();
 		updateUsage(this);
+	}
+	
+	protected void onDestroy(){
+		super.onDestroy();
+		uiHelper.onDestroy();
+	}
+	
+	protected void onSaveInstanceState(Bundle outState) {
+	    super.onSaveInstanceState(outState);
+	    uiHelper.onSaveInstanceState(outState);
+	} 
+
+	public void onPause() {
+	    super.onPause();
+	    uiHelper.onPause();
 	}
 
 	@Override
@@ -147,5 +181,57 @@ public class MainActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	public static String printKeyHash(Activity context) {
+		PackageInfo packageInfo;
+		String key = null;
+		try {
+
+			//getting application package name, as defined in manifest
+			String packageName = context.getApplicationContext().getPackageName();
+
+			//Retriving package info
+			packageInfo = context.getPackageManager().getPackageInfo(packageName,
+					PackageManager.GET_SIGNATURES);
+			
+			Log.e("Package Name=", context.getApplicationContext().getPackageName());
+			
+			for (Signature signature : packageInfo.signatures) {
+				MessageDigest md = MessageDigest.getInstance("SHA");
+				md.update(signature.toByteArray());
+				key = new String(Base64.encode(md.digest(), 0));
+			
+				// String key = new String(Base64.encodeBytes(md.digest()));
+				Log.d("debug","Key Hash="+key);
+
+			}
+		} catch (NameNotFoundException e1) {
+			Log.e("Name not found", e1.toString());
+		}
+
+		catch (NoSuchAlgorithmException e) {
+			Log.e("No such an algorithm", e.toString());
+		} catch (Exception e) {
+			Log.e("Exception", e.toString());
+		}
+
+		return key;
+	}
+	
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	    super.onActivityResult(requestCode, resultCode, data);
+
+	    uiHelper.onActivityResult(requestCode, resultCode, data, new FacebookDialog.Callback() {
+	        @Override
+	        public void onError(FacebookDialog.PendingCall pendingCall, Exception error, Bundle data) {
+	            Log.e("Activity", String.format("Error: %s", error.toString()));
+	        }
+
+	        @Override
+	        public void onComplete(FacebookDialog.PendingCall pendingCall, Bundle data) {
+	            Log.i("Activity", "Success!");
+	        }
+	    });
 	}
 }
